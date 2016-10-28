e02127e0947a5bb569c5df8f1f739da17caa4d3d
Jonathan Sumulong <email": null>
2016-10-28 16:38:35 -0500

    add adjust get_card method in the Round class

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,10 +1,12 @@
 class Round < ActiveRecord::Base
   belongs_to :deck
 
+# grab a random card that has not been guessed correctly!
   def get_card
-    random_num = self.deck.cards.length - 1
-    self.deck.cards
-    .find(random_num)
+    deck = []
+    self.guesses.each do |guess|
+      deck << guess.card unless guess.correct
+    end
+    deck[rand(deck.length)]
   end
-
 end
