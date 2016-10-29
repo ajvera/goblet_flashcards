77d6d204eb866d3ed864ecee22e713cfa160ff0b
Sam Hopkins <hopkins.571@gmail.com>
2016-10-29 18:27:11 -0500

    implement feedback, game_over logic

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -5,35 +5,47 @@ end
 
 get '/round/:id' do
   @round = Round.find(params[:id])
+  redirect "/round/#{params[:id]}/stats" if @round.game_over?
   @card = @round.get_card
+  @feedback = params[:feedback]
   erb :'/rounds/show'
 end
 
 # Problem: @previous_question_feedback is NOT accessible after redirect!!!
 post '/guesses' do
- @round = Round.find_by(id: params[:round_id])
- @card = Card.find_by(id: params[:card_id])
- 
- # if the answer is correct and this is NOT the first guess on that card...
- if @card.answer == params[:answer] && @round.guesses.where(first_guess: false).any?
- 	@previous_question_feedback = "Correct!"
- 	Guess.create(round_id: @round.id, card_id: @card.id, correct: true, first_guess: false)
- 
- # if the answer is correct and this is the first guess on that card...
- elsif @card.answer == params[:answer]
- 	@previous_question_feedback = "Correct! And on your first try!!!"
- 	Guess.create(round_id: @round.id, card_id: @card.id, correct: true, first_guess: true)
- 
- # if the answer is incorrect... 
- else
- 	@previous_question_feedback = "Wrong! The correct answer was: #{@card.answer}"
- 	Guess.create(round_id: @round.id, card_id: @card.id, correct: false, first_guess: false)
- end
+	@round = Round.find_by(id: params[:round_id])
+	@card = Card.find_by(id: params[:card_id])
+	unless @round.game_over?
+	 
+	 # if the answer is correct and this is NOT the first guess on that card...
+	 if @card.answer.downcase == params[:answer].downcase.rstrip && @round.guesses.where(card_id: params[:card_id]).any?
+	 	@previous_question_feedback = "Correct!"
+	 	Guess.create(round_id: @round.id, card_id: @card.id, correct: true, first_guess: false)
+	 
+	 # if the answer is correct and this is the first guess on that card...
+	 elsif @card.answer.downcase == params[:answer].downcase.rstrip && @round.guesses.where(card_id: params[:card_id]).none?
+	 	@previous_question_feedback = "Correct! And on your first try!!!"
+	 	Guess.create(round_id: @round.id, card_id: @card.id, correct: true, first_guess: true)
+	 
+	 # if the answer is incorrect on first try... 
+	 elsif @card.answer.downcase != params[:answer].downcase.rstrip && @round.guesses.where(card_id: params[:card_id]).none?
+	 	@previous_question_feedback = "Wrong! The correct answer was: #{@card.answer}"
+	 	Guess.create(round_id: @round.id, card_id: @card.id, correct: false, first_guess: true)
 
-# PSEUDOCODE GAME LOGIC AFTER USER GUESS:
-#  if correct, send success true query params,
-#   if incorrect, send success false param with correct answer included
-#     redirect to /round/:id with query params
-#     modify get route to look for these params and pass them to the view
-	redirect "/round/#{@round.id}"
+	 else
+	 	@previous_question_feedback = "Keep trying! The correct answer was: #{@card.answer}"
+	 	Guess.create(round_id: @round.id, card_id: @card.id, correct: false, first_guess: false)
+
+	 end
+
+	# PSEUDOCODE GAME LOGIC AFTER USER GUESS:
+	#  if correct, send success true query params,
+	#   if incorrect, send success false param with correct answer included
+	#     redirect to /round/:id with query params
+	#     modify get route to look for these params and pass them to the view
+		redirect "/round/#{@round.id}/stats" if @round.game_over
+		redirect "/round/#{@round.id}?feedback=#{@previous_question_feedback}"
+	else
+		redirect "/"
+	end
 end
--- db/migrate/20161028120518_create_guesses.rb
+++ db/migrate/20161028120518_create_guesses.rb
@@ -3,8 +3,8 @@ class CreateGuesses < ActiveRecord::Migration
   	create_table :guesses do |t|
   		t.integer :round_id, null: false
   		t.integer :card_id, null: false
-  		t.boolean :correct, null: false, default: false
-  		t.boolean :first_guess, null: false, default: true
+  		t.boolean :correct, default: false
+  		t.boolean :first_guess, default: true
 
   		t.timestamps(null: false)
   	end
