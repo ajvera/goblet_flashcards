4ca7362a054666ba0ef2f4b6bdd1bffcd3734b7b
Sam Hopkins <hopkins.571@gmail.com>
2016-10-30 00:25:44 -0500

    refactor seeds, add extra decks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,41 @@
+require 'faker'
+
 Deck.delete_all
 Round.delete_all
 Card.delete_all
 Guess.delete_all
 
 
-deck = Deck.create(name: "Waleetas Trivia Questions")
-card1 = Card.create(question:"What is the opposite of nocturnal?",answer:"diurnal",deck_id:deck.id)
-card2 = Card.create(question:"What do the towns people in the movie Tremors call the monsters?",answer:"Graboids",deck_id:deck.id)
-card3 = Card.create(question:"What year were women in the US legally granted the right to vote?",answer:"1920",deck_id:deck.id)
-card4 = Card.create(question:"What was the original name of the Department of Defense?",answer:"Department of War",deck_id:deck.id)
-card5 = Card.create(question:"What city was Duke Ellington born in?",answer:"Washington D.C.",deck_id:deck.id)
-round = Round.create(deck_id: deck.id)
+
+Deck.create( { name: "Waleetas Trivia Questions",
+							 cards: [	Card.new(question:"What is the opposite of nocturnal?", answer: "diurnal"),
+												Card.new(question:"What do the towns people in the movie Tremors call the monsters?", answer: "Graboids"),
+												Card.new(question:"What year were women in the US legally granted the right to vote?", answer: "1920"),
+												Card.new(question:"What was the original name of the Department of Defense?", answer: "Department of War"),
+												Card.new(question:"What city was Duke Ellington born in?", answer: "Washington D.C.")
+											 ]
+						} )
+
+Deck.create({ name: "World Capitals",
+							cards: [Card.new(question: "Canada", answer: "Ottawa"),
+											Card.new(question: "Austria", answer: "Vienna"),
+											Card.new(question: "Turkey", answer: "Ankara"),
+											Card.new(question: "Ethiopia", answer: "Addis Ababa"),
+											Card.new(question: "Nepal", answer: "Kathmandu"),
+											Card.new(question: "Estonia", answer: "Tallinn"),
+											Card.new(question: "Vietnam", answer: "Hanoi"),
+											Card.new(question: "Honduras", answer: "Tegucigalpa")]
+						} )
+
+Deck.create({ name: "Hipster Gibberish Challenge!",
+							cards: [	Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word),
+												Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word),
+												Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word),
+												Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word),
+												Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word),
+												Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word),
+												Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word),
+												Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word),
+												Card.new(question: "#{Faker::Hipster.sentence.chop}?", answer: Faker::Hipster.word)
+							]
+	})
